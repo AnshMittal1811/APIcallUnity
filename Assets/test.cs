2fd0fcc973b096114945c297b6e7d34c331c93d5
AnshMittal1811 <anshm18111996@gmail.com>
2019-11-18 00:43:48 +0530

    first API call with Unity

---
+++ Assets/test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class test : MonoBehaviour
+{
+    IEnumerator Request()
+    {
+        string url = "www.unity.com";
+        WWW www = new WWW(url);
+        yield return www;
+
+        string html = www.text;
+        print(html);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Request");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
